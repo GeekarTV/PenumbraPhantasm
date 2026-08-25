d68d358ab0831b97cbe3d3c825dfebfcc8c62daa
SGUDestiny <panormov2006@gmail.com>
2026-08-26 00:02:22 +0400

    Hearth error messages

--- src/main/java/destiny/penumbra_phantasm/server/item/SoulHearthItem.java
+++ src/main/java/destiny/penumbra_phantasm/server/item/SoulHearthItem.java
@@ -24,12 +24,10 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import net.minecraftforge.common.util.LazyOptional;
@@ -40,9 +38,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
-
-//TODO:
-// - Transition soul hearth stuff to the capability
 
 public class SoulHearthItem extends Item {
     public static final String OWNER_UUID = "owner_uuid";
